8b1099593e3e84af32e469f559eaec41165a28fb
Akshata Kanaje <akshatakanaje@Akshatas-MacBook-Pro.local>
2023-06-13 15:43:51 -0700

    added login and sign-up and user-profile

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  public profileForm:FormGroup;
+
+  constructor( private router:Router) { 
+    this.profileForm = new FormGroup({
+      'email': new FormControl(null,[Validators.required,Validators.email]),
+      'fullName':  new FormControl(null),
+      'contact':  new FormControl(null),
+      'city':  new FormControl(null),
+      'country':  new FormControl(null),
+      'image':  new FormControl(null),
+      'password':  new FormControl(null),
+      'pincode':  new FormControl(null),
+      'state':  new FormControl(null),
+      'userId':  new FormControl(null)
+    });
+    // this.user = this.authService.getUser();
+    // this.profileForm.patchValue(this.user);
+  }
 
   ngOnInit(): void {
   }
